d49d91d18cc23c0887c668c1a7dbde4912a0011b
cpp-benchmark1 <cpp.benchmark@proton.me>
2025-01-21 17:09:51 +0000

    Update versionconfig.cmake

--- cmake/common/versionconfig.cmake
+++ cmake/common/versionconfig.cmake
@@ -45,10 +45,6 @@ elseif(_obs_version MATCHES "[0-9]+\\.[0-9]+\\.[0-9]+-beta[0-9]+")
   string(REGEX REPLACE "[0-9]+\\.[0-9]+\\.[0-9]+-beta([0-9]+).*$" "\\1" _obs_beta ${_obs_version})
 endif()
 
-list(GET _obs_version_canonical 0 OBS_VERSION_MAJOR)
-list(GET _obs_version_canonical 1 OBS_VERSION_MINOR)
-list(GET _obs_version_canonical 2 OBS_VERSION_PATCH)
-
 set(OBS_RELEASE_CANDIDATE ${_obs_release_candidate})
 set(OBS_BETA ${_obs_beta})
 
